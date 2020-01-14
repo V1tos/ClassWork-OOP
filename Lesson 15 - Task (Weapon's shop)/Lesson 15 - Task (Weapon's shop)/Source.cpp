ac1f69a8ee5697039ea9e27ccb845924f6528024
V1tos <victor.palamarchuk@gmail.com>
2020-01-14 11:10:43 +0200

    Edit Weapon

--- Lesson 15 - Task (Weapon's shop)/Lesson 15 - Task (Weapon's shop)/Source.cpp
+++ Lesson 15 - Task (Weapon's shop)/Lesson 15 - Task (Weapon's shop)/Source.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <string>
+#include <ctime>
 
 using namespace std;
 //Напишіть програму з трьома класами - автомат, магазин, набій.
@@ -25,27 +26,133 @@ class Magazine
 private:
 	class Bullet
 	{
-		unsigned short bulletID;
 		static unsigned int counter;
 
 	public:
-		Bullet() 
+		Bullet *nextBullet;
+		unsigned int bulletID;
+		
+		
+		Bullet(Bullet *nextBullet=nullptr) 
 		{
 			counter++;
 			this->bulletID = counter;
+			this->nextBullet = nextBullet;
+			
 		};
-		void Shot() {
-
+		void ShowBullet() {
+			cout << "Bullet N" << bulletID;
 		}
-		~Bullet();
+
+		~Bullet() {};
 
 	};
 
 	
+	Bullet *head;
+	short magazineSize;
+	short bulletsCount;
+	void ChargeMagazine() {
+		
+		while (bulletsCount < magazineSize)
+		{
+
+			if (this->head == nullptr) {
+				this->head = new Bullet;
+				bulletsCount++;
+			}
+			else
+			{
+				Bullet *current = this->head;
+				while (current->nextBullet != nullptr)
+				{
+					current = current->nextBullet;
+				}
+				current->nextBullet = new Bullet;
+				bulletsCount++;
+
+			}
+
+		}
+
+
+	}
+
 
 public:
-	Magazine();
-	~Magazine();
+	Magazine() { 
+		this->head = nullptr;
+		this->bulletsCount = 0;
+		this->magazineSize = 30;
+		this->ChargeMagazine();
+		
+	};
+	void RechargeMagazine() {
+		if (bulletsCount < magazineSize-1) {
+			
+			while (bulletsCount < magazineSize)
+			{
+
+				if (this->head == nullptr) {
+					this->head = new Bullet;
+					bulletsCount++;
+				}
+				else
+				{
+					Bullet *current = this->head;
+					while (current->nextBullet != nullptr)
+					{
+						current = current->nextBullet;
+					}
+					current->nextBullet = new Bullet;
+					bulletsCount++;
+
+				}
+
+			}
+		}
+		else
+		{
+			cout << "Magazine has been full charged\n";
+		}
+	
+		
+		
+	}
+
+	void GiveBullet(int index) {
+		if (index-1 == 0) {
+			Bullet *tmp = this->head;
+			this->head = this->head->nextBullet;
+			delete tmp;
+			bulletsCount--;
+		}
+		else {
+			Bullet  *prev = this->head;
+			for (int i = 0; i < index - 2; i++) {
+				prev = prev->nextBullet;
+			}
+
+			Bullet  *toDel = prev->nextBullet;
+			prev->nextBullet = toDel->nextBullet;
+			delete toDel;
+			bulletsCount--;
+		}
+	}
+
+	void ShowInfo() {
+		Bullet *current = this->head;
+
+		while (current!=0)
+		{
+			current->ShowBullet(); cout << endl;
+			current = current->nextBullet;
+		}
+	}
+	~Magazine() {};
+	
+
+
 
 
 
@@ -55,8 +162,15 @@ unsigned int Magazine::Bullet::counter = 0;
 
 
 int main() {
-	system("cls");
-
+	srand(unsigned(time(NULL)));
+	int a = rand() % 29 + 1;
+	
+	Magazine maga;
+	maga.GiveBullet(a);
+	/*maga.ShowInfo();*/
+	/*maga.ChargeMagazine();*/
+	/*maga.ShowInfo();*/
+	
 
 
 
