b251dac773e16fadc33ba5c71645c1e125018df3
Viktor Palamarchuk <palamarchuk@rivne.itstep.org>
2020-01-27 14:02:12 +0200

    Stack, Deque

---
+++ Lesson 22 - STL(Stack, Deque)/Lesson 22 - STL(Stack, Deque)/Source.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <deque>
+
+using namespace std;
+
+
+
+int main() {
+
+	//deque <int> container = { 8,5,4 };
+
+	//deque <int>::iterator iter;
+
+	//iter = container.begin();
+
+	//for (int i = 0; i <container.size() ; i++)
+	//{
+	//	cout << iter[i] << endl;
+	//}
+
+
+
+	/*stack <int, vector<int>> arr;
+	arr.push(4);
+	arr.push(7);
+	arr.push(9);
+	arr.push(3);
+	arr.push(6);
+
+	auto test = arr._Get_container();
+
+	cout << test[2];*/
+
+
+
+
+	//stack<string, vector<string>> arr;
+	//arr.push("4asd");
+	//arr.push("asd");
+	//arr.push("7a8d");
+	///*arr.push(6);
+	//arr.push(9);
+	//arr.push(5);*/
+
+
+	//auto containerType = arr._Get_container();
+	//cout << "containerElement => " << containerType[2] << endl;
+	//cout << "Size => " << arr.size() << endl;
+	//cout << "Top element => " << arr.top() << endl;
+	//arr.pop();
+	//cout << "Top element => " << arr.top() << endl;
+	//cout << "All elements " << endl;
+	//while (!arr.empty()) {
+	//	cout << arr.top() << endl;
+	//	arr.pop();
+	//}
+
+	stack<double, vector<double>> arr;
+
+
+
+	system("pause");
+	return 0;
+}
